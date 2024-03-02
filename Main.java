09b438a8bae9923c65b8f4929a7a32b209768b66
Ben3940 <bty2015@comcast.net>
2024-03-01 22:20:45 -0500

    Reads in file with matrix dimensions and begins parsing the file line by line

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        
+        try {
+            File file = new File("matrixDimensions.txt");
+            Scanner scanner = new Scanner(file);
+            ArrayList<Integer> dimensions = new ArrayList<Integer>();
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                dimensions.add(Integer.valueOf(line));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e){
+            System.out.println("No file named 'matrixDimensions.txt' found");
+        } catch (NumberFormatException e){
+            System.out.println("Input file contains non-numerical char");
+        }
     }
 }
