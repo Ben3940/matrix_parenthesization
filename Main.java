fafc230a34ea5b063168176e34a1046fe86eba8f
Ben3940 <bty2015@comcast.net>
2024-03-03 12:42:49 -0500

    Implemented matrix chain multiplication algorithm.  Outputs results table with minimum multiplication counts for matrices

--- Main.java
+++ Main.java
@@ -15,40 +15,48 @@ public class Main{
                 dimensions.add(Integer.valueOf(line));
             }
             int n = dimensions.size() - 1;
-
+            System.out.println(n);
             // Create multiplication counts array with initial values of 0
-            int[][] m = new int[n][n];
-            int[][] s = new int[n][n];
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
+            int[][] m = new int[n+1][n+1];
+            int[][] s = new int[n+1][n+1];
+            for (int i = 0; i <= n; i++){
+                for (int j = 0; j <= n; j++){
                     m[i][j] = 0;
                     s[i][j] = 0;
                 }
             }
 
             // Consider L consecutive matrices to parenthesize
-            for (int L = 2; L < n; L++){
+            for (int L = 2; L <= n; L++){
                 // Split matricies into L pairs, from first matrix to n - L + 1 matrix
-                for (int i = 1; i < n - L + 1; i++){
+                for (int i = 1; i <= (n - L + 1); i++){
                     int j = i + L - 1;
                     
                     // Considers "k" ways to parenthesize matrices given L pairs((L = 2, k = 1), (L = 3, k = 2), etc.)
-                    for (int k = i; k < j - 1; k++){
+                    for (int k = i; k < j; k++){
                         int multi_counts = m[i][k] + m[k+1][j] + dimensions.get(i-1) * dimensions.get(k) * dimensions.get(j);
-                        System.out.println("L: " + L + " k: " + k + " Counts: " + multi_counts);
+                        // System.out.println("L: " + L + " j: " + j + " k: " + k + " Counts: " + multi_counts);
                         if (m[i][j] != 0){
                             m[i][j] = Math.min(multi_counts, m[i][j]);
                             s[i][j] = k;
                         } else {
                             m[i][j] = multi_counts;
+                            s[i][j] = k;
                         }
                     }
                 }
             }
             
-            for (int i = 0; i < m.length;i++){
-                for (int j = 0; j < m[i].length; j++){
-                    System.out.print(m[i][j] + " ");
+            for (int i = 1; i < m.length;i++){
+                for (int j = 1; j < m[i].length; j++){
+                    System.out.printf("%5d ", m[i][j]);
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+            for (int i = 1; i < s.length;i++){
+                for (int j = 1; j < s[i].length; j++){
+                    System.out.print(s[i][j] + " ");
                 }
                 System.out.println("");
             }
