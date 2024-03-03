87d09c1e0d7bf86a909987e175b8696b0c4d3da9
Ben3940 <bty2015@comcast.net>
2024-03-02 19:52:51 -0500

    Began implementing multiplications between L consecutive matrices

--- Main.java
+++ Main.java
@@ -14,6 +14,45 @@ public class Main{
                 line = scanner.nextLine();
                 dimensions.add(Integer.valueOf(line));
             }
+            int n = dimensions.size() - 1;
+
+            // Create multiplication counts array with initial values of 0
+            int[][] m = new int[n][n];
+            int[][] s = new int[n][n];
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    m[i][j] = 0;
+                    s[i][j] = 0;
+                }
+            }
+
+            // Consider L consecutive matrices to parenthesize
+            for (int L = 2; L < n; L++){
+                // Split matricies into L pairs, from first matrix to n - L + 1 matrix
+                for (int i = 1; i < n - L + 1; i++){
+                    int j = i + L - 1;
+                    
+                    // Considers "k" ways to parenthesize matrices given L pairs((L = 2, k = 1), (L = 3, k = 2), etc.)
+                    for (int k = i; k < j - 1; k++){
+                        int multi_counts = m[i][k] + m[k+1][j] + dimensions.get(i-1) * dimensions.get(k) * dimensions.get(j);
+                        System.out.println("L: " + L + " k: " + k + " Counts: " + multi_counts);
+                        if (m[i][j] != 0){
+                            m[i][j] = Math.min(multi_counts, m[i][j]);
+                            s[i][j] = k;
+                        } else {
+                            m[i][j] = multi_counts;
+                        }
+                    }
+                }
+            }
+            
+            for (int i = 0; i < m.length;i++){
+                for (int j = 0; j < m[i].length; j++){
+                    System.out.print(m[i][j] + " ");
+                }
+                System.out.println("");
+            }
+
             scanner.close();
         } catch (FileNotFoundException e){
             System.out.println("No file named 'matrixDimensions.txt' found");
@@ -22,18 +61,4 @@ public class Main{
         }
     }
 
-    public void Matrix_Chain_Order(ArrayList<Integer> p){
-        int n = p.size() - 1;
-
-        // Create multiplication counts array with initial values of 0
-        int[][] m = new int[n][n];
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                m[i][j] = 0;
-            }
-        }
-
-        
-
-    }
 }
